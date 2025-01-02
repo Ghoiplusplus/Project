215d79947232d5b4eb6bc6c4e150dd3d2143473f
PavelKryat <dudapavel82@gmail.com>
2025-01-02 23:22:44 +0300

    Add files via upload

---
+++ mod_telegram/commands/start.py
@@ -0,0 +1,58 @@
+from aiogram import types
+from aiogram.types import KeyboardButton, KeyboardButtonPollType
+from aiogram.utils.keyboard import (ReplyKeyboardMarkup, InlineKeyboardMarkup, InlineKeyboardBuilder,
+                                    KeyboardButton, InlineKeyboardButton, ReplyKeyboardBuilder
+                                    )
+
+
+async def start(message: types.Message) -> None:
+    menu_builder = ReplyKeyboardBuilder()
+    menu_builder.button(
+        text='Помощь'
+    )
+    menu_builder.add(
+        KeyboardButton(text='Отправить контакт',request_contact=True)
+    )
+    menu_builder.row(
+        KeyboardButton(text='Отправить голосование', request_poll=KeyboardButtonPollType())
+    )
+    await message.answer(
+        'Меню',
+        reply_markup=menu_builder.as_markup()
+    )
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
