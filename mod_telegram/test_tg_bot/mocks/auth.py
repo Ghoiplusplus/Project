d46a54f55463e5eb44b836c51b482005e1b669c8
PavelKryat <dudapavel82@gmail.com>
2025-01-11 19:17:23 +0300

    Add files via upload
    внесены корректировки

--- mod_telegram/test_tg_bot/mocks/auth.py
+++ mod_telegram/test_tg_bot/mocks/auth.py
@@ -1,15 +1,15 @@
-# проверка токена (эмуляция)
+# проверка токена
 def check_token(token):
     if token == "valid_token":
         return {"status": "success", "access_token": "mock_access_token", "refresh_token": "mock_refresh_token"}
     else:
         return {"status": "error", "message": "Invalid token"}
 
-# выход из система (эмуляция)
+# выход из системы
 def logout(refresh_token):
     return {"status": "success", "message": "Logged out"}
 
-# авторизация (эмуляция)
+# авторизация
 def authorize_user(login_token):
     if login_token:
         return {"status": "success", "access_token": "mock_access_token", "refresh_token": "mock_refresh_token"}
